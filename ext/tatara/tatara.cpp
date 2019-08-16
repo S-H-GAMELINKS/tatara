3b374c0d8d125cd823f99d0518c4c77ac6cc86e1
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-08-16 19:04:35 +0900

    add Veci class #6

--- ext/tatara/tatara.cpp
+++ ext/tatara/tatara.cpp
@@ -2,10 +2,15 @@
 #include <rice/Data_Type.hpp>
 #include <rice/Constructor.hpp>
 
+#include "vector/veci.hpp"
+
 using namespace Rice;
 
 extern "C" {
     void Init_tatara() {
         Rice::Module rb_mTatara = define_module("Tatara");
+
+        Data_Type<Veci> rb_cVeci = define_class_under<Veci>(rb_mTatara, "Veci")
+            .define_constructor(Constructor<Veci>());
     }
 } 
---
+++ ext/tatara/vector/veci.hpp
@@ -0,0 +1,9 @@
+#ifndef VECI_H_
+#define VECI_H_
+#include <vector>
+
+class Veci {
+    std::vector<int> container;
+};
+
+#endif
