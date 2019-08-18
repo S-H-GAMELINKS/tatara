dbd315365fae9ae48aed1647fcb616c6dda3a72c
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-08-18 21:55:45 +0900

    add test vecf last #15

--- test/vector/vecf_test.rb
+++ test/vector/vecf_test.rb
@@ -12,4 +12,12 @@ class VecfTest < Minitest::Test
     @f.emplace_back(4.2)
     assert_equal @f.first, 4.2
   end
+
+  def test_tatara_vecf_last
+    @f = Tatara::Vecf.new
+    @f.emplace_back(4.2)
+    assert_equal @f.last, 4.2
+    @f.emplace_back(5.5)
+    assert_equal @f.last, 5.5
+  end
 end
