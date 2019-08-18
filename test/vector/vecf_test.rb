4f1bfe244aac1b3afdb12f98a306a31252f90534
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-08-18 21:54:53 +0900

    add test vecf first test #15

--- test/vector/vecf_test.rb
+++ test/vector/vecf_test.rb
@@ -6,4 +6,10 @@ class VecfTest < Minitest::Test
   def test_init_tatara_vecf
     refute_nil Tatara::Vecf.new
   end
+
+  def test_tatara_vecf_first
+    @f = Tatara::Vecf.new
+    @f.emplace_back(4.2)
+    assert_equal @f.first, 4.2
+  end
 end
