168156e13b43ae8a686bbb9ad2dc667b9d879127
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-08-19 12:32:28 +0900

    add & using template Vector class #20

---
+++ ext/tatara/vector/vector.hpp
@@ -0,0 +1,55 @@
+#ifndef VEC_TEMPLATE_H_
+#define VEC_TEMPLATE_H_
+
+#include <vector>
+
+template <class T>
+class Vector {
+    std::vector<T> container;
+
+    public:
+        T first();
+        T last();
+        T bracket(const int index);
+        T bracket_equal(const int index, const T var);
+        void emplace_back(const T var);
+        int size();
+        void clear();
+};
+
+template <class T>
+T Vector<T>::first() {
+    return this->container.front();
+}
+
+template <class T>
+T Vector<T>::last() {
+    return this->container.back();
+}
+
+template <class T>
+T Vector<T>::bracket(const int index) {
+    return this->container[index];
+}
+
+template <class T>
+T Vector<T>::bracket_equal(const int index, const T var) {
+    return this->container[index] = var;
+}
+
+template <class T>
+void Vector<T>::emplace_back(const T var) {
+    this->container.emplace_back(std::move(var));
+}
+
+template <class T>
+int Vector<T>::size() {
+    return this->container.size();
+}
+
+template <class T>
+void Vector<T>::clear() {
+    this->container.clear();
+}
+
+#endif
