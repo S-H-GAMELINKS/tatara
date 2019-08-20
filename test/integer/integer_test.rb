5212ced3bc8d33c9d3aa2da1234c62ea3dbca79e
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-08-20 23:13:49 +0900

    add test_assignment_tatara_integer #10

--- test/integer/integer_test.rb
+++ test/integer/integer_test.rb
@@ -6,4 +6,10 @@ class IntegerTest < Minitest::Test
   def test_init_tatara_integer
     refute_nil Tatara::Integer.new
   end
+
+  def test_assignment_tatara_integer
+    @i = Tatara::Integer.new
+    @i = 42
+    assert_equal @i, 42
+  end
 end
