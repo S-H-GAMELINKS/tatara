587f043a850d258f9d2574a9dd0563c21ad7b91c
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-08-21 22:44:24 +0900

    add test_assignment_tatara_string #9

--- test/string/string_test.rb
+++ test/string/string_test.rb
@@ -6,4 +6,10 @@ class StringTest < Minitest::Test
   def test_init_tatara_string
     refute_nil Tatara::String.new
   end
+
+  def test_assignment_tatara_string
+    @s = Tatara::String.new
+    @s.value = "42"
+    assert_equal @i.value, "42"
+  end
 end
