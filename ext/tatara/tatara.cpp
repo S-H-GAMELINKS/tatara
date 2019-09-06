b1110a1131fe684dec93f104a36a1501f8d5d29c
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-09-07 02:24:36 +0900

    remove Args #77

--- ext/tatara/tatara.cpp
+++ ext/tatara/tatara.cpp
@@ -15,34 +15,34 @@ extern "C" {
         Data_Type<Integer> rbcInteger = define_class_under<Integer>(rb_mTatara, "Integer")
             .define_constructor(Constructor<Integer>())
             .define_method("value", &Integer::return_value)
-            .define_method("value=", &Integer::assignment, Arg("var"))
-            .define_method("value+=", &Integer::plus_equal, Arg("var"))
+            .define_method("value=", &Integer::assignment)
+            .define_method("value+=", &Integer::plus_equal)
             .define_method("inc", &Integer::increment_value)
             .define_method("dec", &Integer::decrement_value)
             .define_method("to_s", &Integer::to_string)
             .define_method("clear", &Integer::clear)
-            .define_method("value==", &Integer::equal, Arg("var"));
+            .define_method("value==", &Integer::equal);
 
         Data_Type<Float> rbcFloat = define_class_under<Float>(rb_mTatara, "Float")
             .define_constructor(Constructor<Float>())
             .define_method("value", &Float::return_value)
-            .define_method("value=", &Float::assignment, Arg("var"))
-            .define_method("value+=", &Float::plus_equal, Arg("var"))
+            .define_method("value=", &Float::assignment)
+            .define_method("value+=", &Float::plus_equal)
             .define_method("inc", &Float::increment_value)
             .define_method("dec", &Float::decrement_value)
             .define_method("to_s", &Float::to_string)
             .define_method("to_i", &Float::to_integer)
             .define_method("clear", &Float::clear)
-            .define_method("value==", &Float::equal, Arg("var"));
+            .define_method("value==", &Float::equal);
 
         Data_Type<CppString> rbcString = define_class_under<CppString>(rb_mTatara, "String")
             .define_constructor(Constructor<CppString>())
             .define_method("value", &CppString::return_value)
-            .define_method("value=", &CppString::assignment, Arg("var"))
-            .define_method("value+=", &CppString::plus_equal, Arg("var"))
+            .define_method("value=", &CppString::assignment)
+            .define_method("value+=", &CppString::plus_equal)
             .define_method("to_i", &CppString::to_integer)
             .define_method("clear", &CppString::clear)
-            .define_method("value==", &CppString::equal, Arg("var"));
+            .define_method("value==", &CppString::equal);
 
         Data_Type<Vector<int>> rb_cVeci = define_class_under<Vector<int>>(rb_mTatara, "Veci")
             .define_constructor(Constructor<Vector<int>>())
@@ -50,7 +50,7 @@ extern "C" {
             .define_method("last", &Vector<int>::last)
             .define_method("[]", &Vector<int>::bracket)
             .define_method("[]=", &Vector<int>::bracket_equal)
-            .define_method("emplace_back", &Vector<int>::emplace_back, Arg("var"))
+            .define_method("emplace_back", &Vector<int>::emplace_back)
             .define_method("size", &Vector<int>::size)
             .define_method("clear", &Vector<int>::clear)
             .define_method("sum", &Vector<int>::sum);
@@ -61,7 +61,7 @@ extern "C" {
             .define_method("last", &Vector<double>::last)
             .define_method("[]", &Vector<double>::bracket)
             .define_method("[]=", &Vector<double>::bracket_equal)
-            .define_method("emplace_back", &Vector<double>::emplace_back, Arg("var"))
+            .define_method("emplace_back", &Vector<double>::emplace_back)
             .define_method("size", &Vector<double>::size)
             .define_method("clear", &Vector<double>::clear)
             .define_method("sum", &Vector<double>::sum);
@@ -72,7 +72,7 @@ extern "C" {
             .define_method("last", &Vector<std::string>::last)
             .define_method("[]", &Vector<std::string>::bracket)
             .define_method("[]=", &Vector<std::string>::bracket_equal)
-            .define_method("emplace_back", &Vector<std::string>::emplace_back, Arg("var"))
+            .define_method("emplace_back", &Vector<std::string>::emplace_back)
             .define_method("size", &Vector<std::string>::size)
             .define_method("clear", &Vector<std::string>::clear)
             .define_method("sum", &Vector<std::string>::sum);
