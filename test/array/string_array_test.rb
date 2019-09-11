82f4a0c515e4c4cfb1bb7d4979242afe222e68cf
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-09-11 22:27:32 +0900

    add test_tatara_string_array_first #8

--- test/array/string_array_test.rb
+++ test/array/string_array_test.rb
@@ -5,4 +5,10 @@ class StringArrayTest < Minitest::Test
   def test_init_tatara_string_array
     refute_nil Tatara::StringArray.new
   end
+
+  def test_tatara_string_array_first
+    @array = Tatara::StringArray.new
+    @array.push("42")
+    assert_equal "42", @array.first
+  end
 end
