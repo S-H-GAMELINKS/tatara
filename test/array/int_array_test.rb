74e342d09fe76cef9480e63d77287c27abb387c5
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-09-11 22:26:40 +0900

    add test_tatara_int_array_first #8

--- test/array/int_array_test.rb
+++ test/array/int_array_test.rb
@@ -5,4 +5,10 @@ class IntArrayTest < Minitest::Test
   def test_init_tatara_int_array
     refute_nil Tatara::IntArray.new
   end
+
+  def test_tatara_int_array_first
+    @array = Tatara::IntArray.new
+    @array.push(42)
+    assert_equal 42, @array.first
+  end
 end
