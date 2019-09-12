c050fcc1ec15c28e26de91075d89030cbbe39a7f
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-09-12 10:18:32 +0900

    add test_tatara_int_array_clear #8

--- test/array/int_array_test.rb
+++ test/array/int_array_test.rb
@@ -33,4 +33,12 @@ class IntArrayTest < Minitest::Test
     @array.push(42)
     assert_equal 1, @array.size
   end
+
+  def test_tatara_int_array_clear
+    @array = Tatara::IntArray.new
+    @array.push(4.2)
+    assert_equal 1, @array.size
+    @array.clear
+    assert_equal 0, @array.size
+  end
 end
