27b12cfb592fb6b31009ceb4fd121e5160adc020
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-09-13 12:54:32 +0900

    rename FloatVector Class test #79

--- test/vector/float_vector_test.rb
+++ test/vector/float_vector_test.rb
@@ -1,50 +1,50 @@
 require "test_helper"
 require "tatara/tatara"
 
-class VecfTest < Minitest::Test
+class FloatVectorTest < Minitest::Test
 
-  def test_init_tatara_vecf
-    refute_nil Tatara::Vecf.new
+  def test_init_tatara_float_vector
+    refute_nil Tatara::FloatVector.new
   end
 
-  def test_tatara_vecf_first
-    @f = Tatara::Vecf.new
+  def test_tatara_float_vector_first
+    @f = Tatara::FloatVector.new
     @f.emplace_back(4.2)
     assert_equal @f.first, 4.2
   end
 
-  def test_tatara_vecf_last
-    @f = Tatara::Vecf.new
+  def test_tatara_float_vector_last
+    @f = Tatara::FloatVector.new
     @f.emplace_back(4.2)
     assert_equal @f.last, 4.2
     @f.emplace_back(5.5)
     assert_equal @f.last, 5.5
   end
 
-  def test_tatara_vecf_bracket
-    @f = Tatara::Vecf.new
+  def test_tatara_float_vector_bracket
+    @f = Tatara::FloatVector.new
     @f.emplace_back(4.2)
     assert_equal @f[0], 4.2
     @f[0] = 5.5
     assert_equal @f[0], 5.5
   end
 
-  def test_tatara_vecf_size
-    @f = Tatara::Vecf.new
+  def test_tatara_float_vector_size
+    @f = Tatara::FloatVector.new
     @f.emplace_back(4.2)
     assert_equal @f.size, 1
   end
 
-  def test_tatara_vecf_clear
-    @f = Tatara::Vecf.new
+  def test_tatara_float_vector_clear
+    @f = Tatara::FloatVector.new
     @f.emplace_back(4.2)
     assert_equal @f.size, 1
     @f.clear
     assert_equal @f.size, 0
   end
 
-  def test_tatara_vecf_sum
-    @f = Tatara::Vecf.new
+  def test_tatara_float_vector_sum
+    @f = Tatara::FloatVector.new
 
     3.times { @f.emplace_back(4.2) }
 
