1733f85b5fcf6f7c9cdebcdda387c999174454f6
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-09-17 12:36:16 +0900

    add test_insert_value_string_int_map #92

--- test/map/string_int_map_test.rb
+++ test/map/string_int_map_test.rb
@@ -5,4 +5,10 @@ class StringIntMapTest < Minitest::Test
   def test_init_tatara_string_int_map
     refute_nil Tatara::StringIntMap.new
   end
+
+  def test_insert_value_string_int_map
+    @map = Tatara::StringIntMap.new
+    @map["Map"] = 42
+    assert_equal 42, @map["Map"]
+    end
 end
