b9c2019f56c6c9cecdfe2b15968bd4a87e65f510
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-09-17 19:08:49 +0900

    add test_insert_value_string_float_map #93

--- test/map/string_float_map_test.rb
+++ test/map/string_float_map_test.rb
@@ -5,4 +5,10 @@ class StringFloatMapTest < Minitest::Test
   def test_init_tatara_string_float_map
     refute_nil Tatara::StringFloatMap.new
   end
+
+  def test_insert_value_string_float_map
+    @map = Tatara::StringFloatMap.new
+    @map["Map"] = 4.2
+    assert_equal 4.2, @map["Map"]
+    end
 end
