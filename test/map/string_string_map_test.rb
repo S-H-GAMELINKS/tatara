e84b037a24f1bb31dd256e322acfbac23ebc7495
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-09-17 19:22:59 +0900

    add test_insert_value_string_string_map #94

--- test/map/string_string_map_test.rb
+++ test/map/string_string_map_test.rb
@@ -5,4 +5,10 @@ class StringStringMapTest < Minitest::Test
   def test_init_tatara_string_string_map
     refute_nil Tatara::StringStringMap.new
   end
+
+  def test_insert_value_string_string_map
+    @map = Tatara::StringStringMap.new
+    @map["Map"] = "42"
+    assert_equal "42", @map["Map"]
+  end
 end
