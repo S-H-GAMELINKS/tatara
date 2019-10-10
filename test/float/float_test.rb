3910fc0c79d5b79e75fff83a51b9ecdda2a1aea7
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-10-10 21:39:00 +0900

    fix test_assignment_tatara_float #167

--- test/float/float_test.rb
+++ test/float/float_test.rb
@@ -13,7 +13,8 @@ class FloatTest < Minitest::Test
     @f = Tatara::Float.new
     @f.value = 4.2
     assert_equal @f.value, 4.2
-    assert_equal 4.2, @f.val
+    @f.val = 2.1
+    assert_equal 2.1, @f.val
   end
 
   def test_increment_tatara_float
