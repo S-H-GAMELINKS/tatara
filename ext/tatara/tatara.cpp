0cd98bd7647eb8b570a0654c8ca6ab59c21b19a8
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-10-22 22:55:26 +0900

    add Integer:val++ method #225

--- ext/tatara/tatara.cpp
+++ ext/tatara/tatara.cpp
@@ -44,6 +44,7 @@ extern "C" {
             .define_method("val**=", &Integer::power_equal)
             .define_method("inc", &Integer::increment_value)
             .define_method("value++", &Integer::increment_value)
+            .define_method("val++", &Integer::increment_value)
             .define_method("dec", &Integer::decrement_value)
             .define_method("to_s", &Integer::to_string)
             .define_method("clear", &Integer::clear)
