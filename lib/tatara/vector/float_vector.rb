04fff14fbdc6a5af6827320e087de8e77ca10e61
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-11-03 16:38:38 +0900

    Fix Tatara::FloatVector#map method #270

--- lib/tatara/vector/float_vector.rb
+++ lib/tatara/vector/float_vector.rb
@@ -3,7 +3,7 @@ require 'tatara/tatara'
 module Tatara
     class FloatVector
         def map(&block)
-            (0...(self.size)).each_with_index{|v, i| block.call(v)}
+            (0...(self.size)).each{|i| block.call(self[i])}
         end
     end
 end
