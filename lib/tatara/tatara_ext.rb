41672b80b99b8c93910a4303eeff84eef2f1a3d9
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-11-06 18:23:51 +0900

    require vector/vector #287

--- lib/tatara/tatara_ext.rb
+++ lib/tatara/tatara_ext.rb
@@ -1,8 +1,6 @@
 
 # require Tatara::Vector's
-require_relative 'vector/int_vector'
-require_relative 'vector/float_vector'
-require_relative 'vector/string_vector'
+require_relative 'vector/vector'
 
 # require Tatara::Array's
 require_relative 'array/int_array'
