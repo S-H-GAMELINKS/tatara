25f3e85485bc1c24199a7f4f6b7e1464c033979e
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-11-06 18:23:01 +0900

    add vector.rb #287

---
+++ lib/tatara/vector/vector.rb
@@ -0,0 +1,33 @@
+require 'tatara/tatara'
+
+module Tatara
+    module Vector
+        def map(&block)
+            (0...(self.size)).each{|i| block.call(self[i])}
+        end
+
+        def map!(&block)
+            (0...(self.size)).each{|i| self[i] = block.call(self[i]) }
+        end
+        
+        def each(&block)
+            (0...(self.size)).each{|i| block.call(self[i])}
+        end
+
+        def each_with_index(&block)
+            (0...(self.size)).each{|i| block.call(self[i], i)}
+        end
+    end
+
+    class IntVector
+        include Vector
+    end
+
+    class FloatVector
+        include Vector
+    end
+
+    class StringVector
+        include Vector
+    end
+end
