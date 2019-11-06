66267bec49ce2889d514fc197357a31e17d4cbf2
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-11-06 18:29:12 +0900

    add array.rb #288

---
+++ lib/tatara/array/array.rb
@@ -0,0 +1,25 @@
+require 'tatara/tatara'
+
+module Tatara
+    module Array
+        def map(&block)
+            (0...(self.size)).each{|i| block.call(self[i])}
+        end
+
+        def map!(&block)
+            (0...(self.size)).each{|i| self[i] = block.call(self[i]) }
+        end
+    end
+
+    class IntArray
+        include Array
+    end
+
+    class FloatArray
+        include Array
+    end
+
+    class StringArray
+        include Array
+    end
+end
