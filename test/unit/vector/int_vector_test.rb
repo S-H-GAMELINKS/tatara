2be3075c36a734562b8a9b5f85d25f571bead449
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-11-14 22:21:25 +0900

    add test_sort_method_int_vector #345

--- test/unit/vector/int_vector_test.rb
+++ test/unit/vector/int_vector_test.rb
@@ -133,4 +133,15 @@ class IntVectorTest < Minitest::Test
 
     @i.map{|i| assert_equal 10, i }
   end
+
+  def test_sort_method_int_vector
+    data = [4, 9, 1]
+    @i = Tatara::IntVector.new
+    data.each(&@i.method(:<<))
+    @i = @i.sort
+    data.sort!
+    data.each_with_index{|v, i|
+      assert_equal v, @i[i]
+    }
+  end
 end
