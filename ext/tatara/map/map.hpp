5ac6dac653ff78b68b74109f83e9b59a74c44b6d
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-11-22 03:34:03 +0900

    fix map class dir #416

--- ext/tatara/map/map.hpp
+++ ext/tatara/map/map.hpp
@@ -2,18 +2,18 @@
 #define MAP_H_
 
 // include String Key Map
-#include "string_int_map.hpp"
-#include "string_float_map.hpp"
-#include "string_string_map.hpp"
+#include "string/string_int_map.hpp"
+#include "string/string_float_map.hpp"
+#include "string/string_string_map.hpp"
 
 // include Int Key Map
-#include "int_int_map.hpp"
-#include "int_float_map.hpp"
-#include "int_string_map.hpp"
+#include "int/int_int_map.hpp"
+#include "int/int_float_map.hpp"
+#include "int/int_string_map.hpp"
 
 // include Float Key Map
-#include "float_int_map.hpp"
-#include "float_float_map.hpp"
-#include "float_string_map.hpp"
+#include "float/float_int_map.hpp"
+#include "float/float_float_map.hpp"
+#include "float/float_string_map.hpp"
 
 #endif
