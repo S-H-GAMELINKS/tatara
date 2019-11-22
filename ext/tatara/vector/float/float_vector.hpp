48abb55e4fd1e6dff6bf5eb05be3fa4844bb776b
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-11-23 02:28:29 +0900

    fix FloatVector class #416

--- ext/tatara/vector/float/float_vector.hpp
+++ ext/tatara/vector/float/float_vector.hpp
@@ -4,12 +4,12 @@
 #include <ruby.h>
 #include <vector>
 
-class FloatString {
+class FloatVector {
     std::vector<double> container;
 
     public:
-        FloatString();
-        ~FloatString();
+        FloatVector();
+        ~FloatVector();
         double first();
         double last();
         double bracket(const int index);
@@ -17,84 +17,84 @@ class FloatString {
         void emplace_back(const double var);
         int size();
         void clear();
-        FloatString& push_back_object(const double var);
+        FloatVector& push_back_object(const double var);
  };
 
-FloatString::FloatString() {}
+FloatVector::FloatVector() {}
 
-FloatString::~FloatString() {}
+FloatVector::~FloatVector() {}
 
-double FloatString::first() {
+double FloatVector::first() {
     return this->container.front();
 }
 
-double FloatString::last() {
+double FloatVector::last() {
     return this->container.back();
 }
 
-double FloatString::bracket(const int index) {
+double FloatVector::bracket(const int index) {
     return this->container[index];
 }
 
-double FloatString::bracket_equal(const int index, const double var) {
+double FloatVector::bracket_equal(const int index, const double var) {
     return this->container[index] = var;
 }
 
-void FloatString::emplace_back(const double var) {
+void FloatVector::emplace_back(const double var) {
     this->container.emplace_back(var);
 }
 
-int FloatString::size() {
+int FloatVector::size() {
     return this->container.size();
 }
 
-void FloatString::clear() {
+void FloatVector::clear() {
     this->container.clear();
 }
 
-FloatString &FloatString::push_back_object(const double var) {
+FloatVector &FloatVector::push_back_object(const double var) {
     this->container.emplace_back(std::move(var));
     return *this;
 }
 
-static FloatString *getFloatString(VALUE self) {
-    FloatString *ptr;
-    Data_Get_Struct(self, FloatString, ptr);
+static FloatVector *getFloatVector(VALUE self) {
+    FloatVector *ptr;
+    Data_Get_Struct(self, FloatVector, ptr);
     return ptr;
 }
 
-static void wrap_float_vector_free(FloatString *ptr) {
-    ptr->~FloatString();
+static void wrap_float_vector_free(FloatVector *ptr) {
+    ptr->~FloatVector();
     ruby_xfree(ptr);
 }
 
 static VALUE wrap_float_vector_alloc(VALUE klass) {
-    void *p = ruby_xmalloc(sizeof(FloatString));
-    p = new FloatString;
+    void *p = ruby_xmalloc(sizeof(FloatVector));
+    p = new FloatVector;
     return Data_Wrap_Struct(klass, NULL, wrap_float_vector_free, p);
 }
 
 static VALUE wrap_float_vector_init(VALUE self) {
-    FloatString *p = getFloatString(self);
-    p = new FloatString;
+    FloatVector *p = getFloatVector(self);
+    p = new FloatVector;
     return Qnil;
 }
 
 static VALUE wrap_float_vector_first(VALUE self) {
-    const double value = getFloatString(self)->first();
+    const double value = getFloatVector(self)->first();
     VALUE result = DBL2NUM(value);
     return result;
 }
 
 static VALUE wrap_float_vector_last(VALUE self) {
-    const double value = getFloatString(self)->last();
+    const double value = getFloatVector(self)->last();
     VALUE result = DBL2NUM(value);
     return result;
 }
 
 static VALUE wrap_float_vector_bracket(VALUE self, VALUE index) {
     const int i = NUM2INT(index);
-    const double value = getFloatString(self)->bracket(i);
+    const double value = getFloatVector(self)->bracket(i);
     VALUE result = DBL2NUM(value);
     return result;
 }
@@ -102,30 +102,30 @@ static VALUE wrap_float_vector_bracket(VALUE self, VALUE index) {
 static VALUE wrap_float_vector_bracket_equal(VALUE self, VALUE index, VALUE value) {
     const int i = NUM2INT(index);
     const double v = NUM2DBL(value);
-    getFloatString(self)->bracket_equal(i, v);
+    getFloatVector(self)->bracket_equal(i, v);
     return value;
 }
 
 static VALUE wrap_float_vector_emplace_back(VALUE self, VALUE value) {
     const double v = NUM2DBL(value);
-    getFloatString(self)->emplace_back(v);
+    getFloatVector(self)->emplace_back(v);
     return Qnil;
 }
 
 static VALUE wrap_float_vector_size(VALUE self) {
-    const int size = getFloatString(self)->size();
+    const int size = getFloatVector(self)->size();
     VALUE result = INT2NUM(size);
     return result;
 }
 
 static VALUE wrap_float_vector_clear(VALUE self) {
-    getFloatString(self)->clear();
+    getFloatVector(self)->clear();
     return Qnil;
 }
 
 static VALUE wrap_float_vector_push_back_object(VALUE self, VALUE value) {
     const double v = NUM2DBL(value);
-    getFloatString(self)->push_back_object(v);
+    getFloatVector(self)->push_back_object(v);
     return self;
 }
 
