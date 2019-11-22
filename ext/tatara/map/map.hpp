e134cbfcc59aeb92fae4afd3aeba0bbdeb380b4d
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-11-23 02:33:55 +0900

    fix dir name #416

--- ext/tatara/map/map.hpp
+++ ext/tatara/map/map.hpp
@@ -7,9 +7,9 @@
 #include "string/string_string_map.hpp"
 
 // include Int Key Map
-#include "int/int_int_map.hpp"
-#include "int/int_float_map.hpp"
-#include "int/int_string_map.hpp"
+#include "integer/int_int_map.hpp"
+#include "integer/int_float_map.hpp"
+#include "integer/int_string_map.hpp"
 
 // include Float Key Map
 #include "float/float_int_map.hpp"
