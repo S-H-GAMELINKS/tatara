b0e6c12cc6e58ea23eb2bd8c6de8bd33e318c39b
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-12-03 23:35:59 +0900

    fix benchmark #517

--- benchmark/benchmark.rb
+++ benchmark/benchmark.rb
@@ -5,10 +5,22 @@ Benchmark.driver do |x|
     require 'tatara/tatara'
   RUBY
 
-  x.report %{ Tatara::String.new }
   x.report %{ Tatara::Integer.new }
   x.report %{ Tatara::Float.new }
-  x.report %{ Tatara::StringVector.new }
+  x.report %{ Tatara::String.new }
   x.report %{ Tatara::IntVector.new }
   x.report %{ Tatara::FloatVector.new }
+  x.report %{ Tatara::StringVector.new }
+  x.report %{ Tatara::IntArray.new }
+  x.report %{ Tatara::FloatArray.new }
+  x.report %{ Tatara::StringArray.new }
+  x.report %{ Tatara::StringIntMap.new }
+  x.report %{ Tatara::StringFloatMap.new }
+  x.report %{ Tatara::StringStringMap.new }
+  x.report %{ Tatara::IntIntMap.new }
+  x.report %{ Tatara::IntFloatMap.new }
+  x.report %{ Tatara::IntStringMap.new }
+  x.report %{ Tatara::FloatIntMap.new }
+  x.report %{ Tatara::FloatFloatMap.new }
+  x.report %{ Tatara::FloatStringMap.new }  
 end
