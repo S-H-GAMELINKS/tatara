be441b7905f0790bbc70e50602c6b92b1df38f97
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-12-06 01:51:56 +0900

    add FloatFloatMap#insert method test #530

--- test/unit/map/float_float_map_test.rb
+++ test/unit/map/float_float_map_test.rb
@@ -11,4 +11,10 @@ class FloatFloatMapTest < Minitest::Test
     @map[0.5] = 4.2
     assert_equal 4.2, @map[0.5]
   end
+
+  def test_insert_float_float_map
+    @map = Tatara::FloatFloatMap.new
+    @map.insert(4.2, 4.2)
+    assert_equal 4.2, @map[4.2]
+  end
 end
