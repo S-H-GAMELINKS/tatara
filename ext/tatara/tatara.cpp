220be2c4fdc042845c8b7350a2cc99c3f9a8c060
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-12-06 22:01:57 +0900

    using alias Tatara::Integer#val%= method #532

--- ext/tatara/tatara.cpp
+++ ext/tatara/tatara.cpp
@@ -37,7 +37,7 @@ extern "C" {
         rb_define_method(rb_cInteger, "value%", RUBY_METHOD_FUNC(wrap_int_mod), 1);
         rb_define_alias(rb_cInteger, "val%", "value%");
         rb_define_method(rb_cInteger, "value%=", RUBY_METHOD_FUNC(wrap_int_mod_equal), 1);
-        rb_define_method(rb_cInteger, "val%=", RUBY_METHOD_FUNC(wrap_int_mod_equal), 1);
+        rb_define_alias(rb_cInteger, "val%=", "value%=");
         rb_define_method(rb_cInteger, "value**", RUBY_METHOD_FUNC(wrap_int_power), 1);
         rb_define_method(rb_cInteger, "val**", RUBY_METHOD_FUNC(wrap_int_power), 1);
         rb_define_method(rb_cInteger, "value**=", RUBY_METHOD_FUNC(wrap_int_power_equal), 1);
