9cd735cc5f1ffe01d01a83f0c83323604bfcbe44
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-12-26 15:55:32 +0900

    Using impl Tatara::Array#sum method #620

--- ext/tatara/tatara.cpp
+++ ext/tatara/tatara.cpp
@@ -191,6 +191,7 @@ extern "C" {
         rb_define_method(rb_cIntArray, "each_with_index", RUBY_METHOD_FUNC(wrap_int_array_each_with_index), 0);
         rb_define_method(rb_cIntArray, "to_array", RUBY_METHOD_FUNC(wrap_int_array_convert_array), 0);
         rb_define_method(rb_cIntArray, "import_array", RUBY_METHOD_FUNC(wrap_int_array_import_array), 1);
+        rb_define_method(rb_cIntArray, "sum", RUBY_METHOD_FUNC(wrap_int_array_sum), 0);
 
         VALUE rb_cFloatArray = rb_define_class_under(mTatara, "FloatArray", rb_cObject);
 
@@ -210,6 +211,7 @@ extern "C" {
         rb_define_method(rb_cFloatArray, "each_with_index", RUBY_METHOD_FUNC(wrap_float_array_each_with_index), 0);
         rb_define_method(rb_cFloatArray, "to_array", RUBY_METHOD_FUNC(wrap_float_array_convert_array), 0);
         rb_define_method(rb_cFloatArray, "import_array", RUBY_METHOD_FUNC(wrap_float_array_import_array), 1);
+        rb_define_method(rb_cFloatArray, "sum", RUBY_METHOD_FUNC(wrap_float_array_sum), 0);
 
         VALUE rb_cStringArray = rb_define_class_under(mTatara, "StringArray", rb_cObject);
 
@@ -229,6 +231,7 @@ extern "C" {
         rb_define_method(rb_cStringArray, "each_with_index", RUBY_METHOD_FUNC(wrap_string_array_each_with_index), 0);
         rb_define_method(rb_cStringArray, "to_array", RUBY_METHOD_FUNC(wrap_string_array_convert_array), 0);
         rb_define_method(rb_cStringArray, "import_array", RUBY_METHOD_FUNC(wrap_string_array_import_array), 1);
+        rb_define_method(rb_cStringArray, "sum", RUBY_METHOD_FUNC(wrap_string_array_sum), 0);
 
         VALUE rb_cStringIntMap = rb_define_class_under(mTatara, "StringIntMap", rb_cObject);
 
