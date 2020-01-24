082de748f38b667814c078a08fb646f755c9cbae
S-H-GAMELINKS <gamelinks007@gmail.com>
2020-01-24 20:42:34 +0900

    Add Tatara::Integer#operator% test #666

--- test/unit/integer/integer_test.rb
+++ test/unit/integer/integer_test.rb
@@ -226,4 +226,11 @@ class IntegerTest < Minitest::Test
     @i = @i1 ** @i2
     assert_equal 16, @i.val    
   end
+
+  def test_mod_object_tatara_integer
+    @i1 = Tatara::Integer.new << 3
+    @i2 = Tatara::Integer.new << 2
+    @i = @i1 % @i2
+    assert_equal 1, @i.val
+  end
 end
