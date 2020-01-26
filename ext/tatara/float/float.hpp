0452c5d183495341d6cd3cc0887749d28d96ee00
S-H-GAMELINKS <gamelinks007@gmail.com>
2020-01-26 22:34:30 +0900

    Add Tatara::Float#operator- #673

--- ext/tatara/float/float.hpp
+++ ext/tatara/float/float.hpp
@@ -245,6 +245,17 @@ static VALUE float_object_plus(VALUE self, VALUE other) {
     }
 }
 
+static VALUE float_object_minus(VALUE self, VALUE other) {
+    if(TYPE(other) == TYPE(self)) {
+        VALUE dup = rb_obj_dup(self);
+        VALUE result = float_minus(self, rb_ivar_get(other, rb_intern("value")));
+        rb_ivar_set(dup, rb_intern("value"), result);
+        return dup;
+    } else {
+        rb_raise(rb_eTypeError, "Worng Type! This Value type is %s !", rb_class_name(other));
+        return Qnil;
+    }
+}
 extern "C" {
     inline void Init_float(VALUE mTatara) {
         VALUE rb_cFloat = rb_define_class_under(mTatara, "Float", rb_cObject);
@@ -284,6 +295,7 @@ extern "C" {
         rb_define_alias(rb_cFloat, "equal?", "value==");
         rb_define_alias(rb_cFloat, "<<", "value=");
         rb_define_method(rb_cFloat, "+", float_object_plus, 1);
+        rb_define_method(rb_cFloat, "-", float_object_minus, 1);
     }
 }
 
