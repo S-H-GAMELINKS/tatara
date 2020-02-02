4096c0354eb46beff31307271f712f4bd134b0ed
S-H-GAMELINKS <gamelinks007@gmail.com>
2020-02-02 15:28:07 +0900

    Add Tatara::Any#value= method test #689

--- test/unit/any/any_test.rb
+++ test/unit/any/any_test.rb
@@ -8,4 +8,15 @@ class AnyTest < Minitest::Test
     refute_nil @a
     assert_nil @a.value
   end
+
+  def test_assignment_tatara_any
+    @a = Tatara::Any.new
+    assert_nil @a.value
+    @a.value = 42
+    assert_equal 42, @a.value
+    @a.value = 4.2
+    assert_equal 4.2, @a.value
+    @a.value = "hello"
+    assert_equal "hello", @a.value
+  end
 end
