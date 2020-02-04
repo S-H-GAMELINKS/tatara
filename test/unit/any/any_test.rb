3719f1da139e793c25da0d4746164564dc8d6798
S-H-GAMELINKS <gamelinks007@gmail.com>
2020-02-04 22:59:39 +0900

    Add Tatara::Any#value- method test #691

--- test/unit/any/any_test.rb
+++ test/unit/any/any_test.rb
@@ -30,4 +30,15 @@ class AnyTest < Minitest::Test
     @a.value = 4.2
     assert_equal 6.3, (@a.value + 2.1).round(2)
   end
+
+  def test_minus_tatara_any
+    @a = Tatara::Any.new
+    assert_nil @a.value
+    @a.value = 42
+    assert_equal 21, @a.value - 21
+    @a.value = "hello"
+    assert_raises(NoMethodError) { @a.value - "hello" }
+    @a.value = 4.2
+    assert_equal 2.1, (@a.value - 2.1).round(2)
+  end
 end
