304b66f13863efe6d78c0cd9953d61480d03add1
S-H-GAMELINKS <gamelinks007@gmail.com>
2020-02-04 22:59:44 +0900

    Add Tatara::Any#value- method #691

--- ext/tatara/any/any.hpp
+++ ext/tatara/any/any.hpp
@@ -34,6 +34,24 @@ static VALUE any_plus(VALUE self, VALUE value) {
     }
 }
 
+static VALUE any_minus(VALUE self, VALUE value) {
+    VALUE ivar = rb_ivar_get(self, rb_intern("value"));
+
+    if (FIXNUM_P(ivar) && FIXNUM_P(value)) {
+        long result = NUM2INT(ivar) + NUM2INT(value);
+        return INT2NUM(result);
+    } else if (TYPE(ivar) == T_FLOAT && TYPE(ivar) == T_FLOAT) {
+        double result = NUM2DBL(ivar) + NUM2DBL(value);
+        return DBL2NUM(result);
+    } else if (TYPE(ivar) == T_STRING && TYPE(value) == T_STRING) {
+        rb_raise(rb_eNoMethodError, "No operator minus for String!!");
+        return rb_str_plus(ivar, value);
+    } else {
+        rb_raise(rb_eTypeError, "Worng Type! This Value type is %s !", rb_class_name(value));
+        return Qnil;
+    }
+}
+
 extern "C" {
     inline void Init_any(VALUE mTatara) {
         VALUE rb_cAny = rb_define_class_under(mTatara, "Any", rb_cObject);
@@ -42,6 +60,7 @@ extern "C" {
         rb_define_method(rb_cAny, "value", any_return_value, 0);
         rb_define_method(rb_cAny, "value=", any_assignment, 1);
         rb_define_method(rb_cAny, "value+", any_plus, 1);
+        rb_define_method(rb_cAny, "value-", any_minus, 1);
     }
 }
 
